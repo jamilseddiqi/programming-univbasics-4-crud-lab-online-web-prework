3c0d29ec35648149d071b98129a867dae66a9bd2
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-15 19:34:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,21 @@
-def create_an_empty_array
+
+my_array = [ ]
+
+favorite_fruits = ["banana", "apple", "peach", "berries"]
   
-end
+favorite_fruits << ""
 
-def create_an_array
+favorite_fruits.unshift ("")
+
+favorite_fruits = favorite_fruits.pop
+
+banana = favorite_fruits.shift 
+
+favorite_fruits = [2]
+
+favorite_fruits = [0]
   
-end
+favorite_fruits = [-1]  
 
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
-end
+favorite_fruits [2] = "banana" 
+  => "grapes"
