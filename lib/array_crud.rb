9545747f44f04e935528c30da9aae58731042d6f
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-15 19:34:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,20 +2,20 @@
 my_array = [ ]
 
 favorite_fruits = ["banana", "apple", "peach", "berries"]
-  
+
 favorite_fruits << ""
 
 favorite_fruits.unshift ("")
 
 favorite_fruits = favorite_fruits.pop
 
-banana = favorite_fruits.shift 
+banana = favorite_fruits.shift
 
 favorite_fruits = [2]
 
 favorite_fruits = [0]
-  
-favorite_fruits = [-1]  
 
-favorite_fruits [2] = "banana" 
+favorite_fruits = [-1]
+
+favorite_fruits [2] = "banana"
   => "grapes"
